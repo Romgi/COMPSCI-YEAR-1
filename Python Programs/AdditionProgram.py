4ebd1656b2717495bd104795c61dbb506d32e824
Romgi Productions <JonathanGraydon22@gmail.com>
2025-09-05 09:09:34 -0400

    Added output message to addition program

--- Python Programs/AdditionProgram.py
+++ Python Programs/AdditionProgram.py
@@ -4,5 +4,5 @@
 def main():
     x = int(input("Enter the first number: "))
     y = int(input("Enter the second number: "))
-    print(x + y)
+    print("The sum of the two numbers is: " + str(x + y))
 main()
