251b0d4708d96e865e55a4373674fcb5582441f4
Romgi Productions <JonathanGraydon22@gmail.com>
2025-09-09 09:02:03 -0400

    Added Boolean.py example

---
+++ Python Programs/Boolean.py
@@ -0,0 +1,24 @@
+# #Simplify the following statements
+# def main():
+#     a or True
+#     a or False
+#     a and True
+#     a and False
+
+
+#     #Add paraenthesis to the expression below to be explicit about order
+#     #of operations.
+#     a and (not b) or (not a) or b
+
+
+#     #For what values of "a" and "b" is the following False/True?
+#     not((a or b) and not(b or not a or not b))
+# main()
+
+
+# a and True
+# a and False
+# a or (not a)
+# a and (not a)
+
+
