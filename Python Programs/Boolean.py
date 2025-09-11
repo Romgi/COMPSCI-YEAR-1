f4b85d269d1304a9a13d5a41d8d63af760b0fb9f
Romgi Productions <JonathanGraydon22@gmail.com>
2025-09-11 08:47:15 -0400

    Finished boolean program

--- Python Programs/Boolean.py
+++ Python Programs/Boolean.py
@@ -12,12 +12,15 @@
 
 
 #     #For what values of "a" and "b" is the following False/True?
-#     not((a or b) and not(b or not a or not b))
+a = True, b = False
+not((a or b) and not(b or not a or not b))
+not((a or b) and not(True))
+not((a or b) and False)
+not(False)
+True
+# True for all possible values of a and b
+
+#Tautology - always true statement
+#Contradiction - always false statement
+
 # main()
-
-# a and True
-# a and False
-# a or (not a)
-# a and (not a)
-
-
