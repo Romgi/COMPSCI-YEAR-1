1861e1fd2ae5ab45f5a91b8b8f4c484ea2c68971
Romgi Productions <JonathanGraydon22@gmail.com>
2025-10-02 16:36:39 -0400

    Started Exercises3

--- COMPSCI-1JC3/Exercises/Exercise03/HaskellExercises03/src/Exercises03.hs
+++ COMPSCI-1JC3/Exercises/Exercise03/HaskellExercises03/src/Exercises03.hs
@@ -36,9 +36,9 @@ macid = "graydj1"
 --------------------------------------------------------------------------------
 fib :: (Integral a) => a -> a
 fib n
-  | n <= 0 = error "TODO implement fib"
-  | n == 1 = error "TODO implement fib"
-  | otherwise = error "TODO implement fib"
+  | n <= 0 = 0
+  | n == 1 = 1
+  | otherwise = fib (n-1) + fib (n-2)
 
 -- Exercise B
 --------------------------------------------------------------------------------
