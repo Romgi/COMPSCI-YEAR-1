6ab810b2e34092f38a0eaacb6a68ded110da3612
Romgi Productions <JonathanGraydon22@gmail.com>
2026-02-24 11:12:51 -0500

    Fixed work for 1xc3 lab 6-2

--- COMPSCI-1XC3/Labs/Lab6-2/For-Loop.c
+++ COMPSCI-1XC3/Labs/Lab6-2/For-Loop.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-
+    // For Loop
     for (int i = 1; i <= 10; i++)
     {
         if (i == 5)
@@ -10,6 +10,21 @@ int main()
             continue;
         }
         printf("%d\n", i);
+    }
+
+    printf("\n");
+
+    // While loop
+    int i = 1;
+    while (i <= 10)
+    {
+        if (i == 5)
+        {
+            i++;
+            continue;
+        }
+        printf("%d\n", i);
+        i++;
     }
 
     return 0;
--- COMPSCI-1XC3/Labs/Lab6-2/Loop-Reasoning.c
+++ COMPSCI-1XC3/Labs/Lab6-2/Loop-Reasoning.c
@@ -2,11 +2,17 @@
 
 int main()
 {
+    int i;
+    int runs = 0;
 
-    for (int i = 0; i <= 5; i++)
+    for (i = 0; i <= 5; i++)
     {
         printf("%d\n", i);
+        runs++;
     }
+
+    printf("Loop runs: %d times\n", runs);
+    printf("Final i after loop: %d\n", i);
 
     return 0;
 }
