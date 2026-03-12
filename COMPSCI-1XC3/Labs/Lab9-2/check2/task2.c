945e16191ec1f3d98b6230740b07de2479e0e6eb
Romgi Productions <JonathanGraydon22@gmail.com>
2026-03-12 10:47:39 -0400

    Simplified and compiled TA check 2

--- COMPSCI-1XC3/Labs/Lab9-2/check2/task2.c
+++ COMPSCI-1XC3/Labs/Lab9-2/check2/task2.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int arr[5] = {1, 2, 3, 4, 5};
     int *ptr = arr;
 
-    for (int i = 0; i < 5; i++) {
-        printf("%d", *(ptr + i));
-        if (i < 4) {
-            printf(" ");
-        }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", *(ptr + i));
     }
-    printf("\n");
 
     return 0;
 }
