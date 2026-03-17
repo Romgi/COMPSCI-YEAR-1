cf98620f890bb03471026602dca894e26710327f
Romgi Productions <JonathanGraydon22@gmail.com>
2026-03-17 13:08:07 -0400

    Added info for connect.php

--- COMPSCI-1XD3/Full-Stack-Example-Pack/Full-Stack-Example-Pack-main/36_php_data_objects/basic_pdo/connect.php
+++ COMPSCI-1XD3/Full-Stack-Example-Pack/Full-Stack-Example-Pack-main/36_php_data_objects/basic_pdo/connect.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Include this to connect. Change the dbname to match your database,
  * and make sure your login information is correct after you upload 
@@ -8,9 +9,9 @@
  */
 try {
     $dbh = new PDO(
-        "mysql:host=localhost;dbname=test",
-        "root",
-        ""
+        "mysql:host=localhost;dbname=graydj1_db",
+        "graydj1_local",
+        ")wt_xR:e"
     );
 } catch (Exception $e) {
     die("ERROR: Couldn't connect. {$e->getMessage()}");
